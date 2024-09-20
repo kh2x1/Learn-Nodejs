1a0112ddc4ce951f82eef6e841e501818d9e7625
kh2x1 <kalad3808@gmail.com>
2024-09-20 10:30:51 +0300

    Link-Mongodb

--- Express.js
+++ Express.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import mongoose from 'mongoose';
+
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,12 +14,18 @@ app.get('/', (req, res) => {
     res.sendFile('/views/index.html', {root: __dirname});
 });
 
-app.listen(3000, () => {
-    console.log(`Server running on http://localhost:${port}`);
+
+
+
+mongoose
+.connect('mongodb+srv://Kh2x1:2tq693bqRrssHAeV@cluster0.ymwfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    app.listen(3000, () => {
+        console.log(`Server running on http://localhost:${port}`);
+    }); })
+.catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
 });
-
-
-
 
 
 
